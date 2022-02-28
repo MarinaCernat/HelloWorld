e021b95872851b301eb8dfe27829f3b062fd810f
Marina <marina.butterfly95@gmail.com>
2022-02-28 17:37:22 +0200

    Fourth commit

--- src/main.java
+++ src/main.java
@@ -1,5 +1,7 @@
 public class main {
     public static void main(String[] args){
         System.out.println("Helloooooooo World!");
+        System.out.print("Helloooooooo!");
+        System.out.println("tananana");
     }
 }
